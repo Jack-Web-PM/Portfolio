785a0ceeea8966663025a973232fb4365e53bb68
Jack-Web-PM <jack.chou24@student.cart.org>
2025-05-13 15:05:19 -0700

    update

--- js/data/projects.js
+++ js/data/projects.js
@@ -2,8 +2,43 @@ export const projects = [
   {
     title: "Project 1",
     description:
-      "Risk and Ruin, our text adventure game created by HTML, CSS, and JavaScript. Also are the most popular text adventure created in 2025.",
+      "Risk and Ruin, A text adventure game project created by HTML, CSS, and JavaScript. Also are the most popular text adventure created in 2025.",
     demoLink: "https://riskandruin.netlify.app/",
     githubLink: "https://github.com/CartWebApp/project-adventure-usurper-frank-the-x-esquire-dev-team",
+  },
+  {
+    title: "Project 2",
+    description:
+      "EECU Budget Calculator, a budget calculator that used to calculate budget.",
+    demoLink: "https://eecubudgetproject.netlify.app/",
+    githubLink: "https://github.com/Jack-Web-PM/EECU-budget-calculator-",
+  },
+  {
+    title: "Project 3",
+    description:
+      "Hurt eyes, a website having the practice of array and loops in JavaScript.",
+    demoLink: "https://hurteyes.netlify.app/",
+    githubLink: "https://github.com/CartWebApp/js-exercise-bouncy-balls-Jack-Web-PM",
+  },
+  {
+    title: "Project 4",
+    description:
+      "Trion Supercars, We as a employer that designed and scatch out an website as a group.",
+    demoLink: "https://example.com/",
+    githubLink: "https://github.com/LukasDekan/fork-supercar-website-pm-team-trion-web",
+  },
+  {
+    title: "Project 5",
+    description:
+      "Smart Home Content, as I start learning coding, the first website/practice I'd made.",
+    demoLink: "https://warm-blancmange-23be81.netlify.app/",
+    githubLink: "https://github.com/Jack-Web-PM/smart-home-content",
+  },
+  {
+    title: "Project 6",
+    description:
+      "Meme Certificate, one of our project which have to designed 3 different meme as a certificate.",
+    demoLink: "https://certificatex3.netlify.app/",
+    githubLink: "https://github.com/Jack-Web-PM/Certificate-designs-x3",
   },
 ];
